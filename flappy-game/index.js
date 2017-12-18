47afecfdf8c0449ac8f816ac61df8919798afd38
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2017-12-18 14:15:08 +0000

    added html,css and created js dom variables

--- flappy-game/index.js
+++ flappy-game/index.js
@@ -11,3 +11,18 @@
 // Resets the game when character hits the bars and alert losing message
 
 // Give a winning score
+
+$(function(){
+
+var container = $('#container');
+var character = $('#character');
+var bar = $('.bar');
+var bar_1 = $('#bar_1');
+var bar_2 = $('#bar_2');
+var score = $('#score');
+var speed = $('#speed');
+var score = $('#score');
+var reset_btn = $('#reset_btn');
+
+
+});

0c6c26726366074112f2b1e7a3d966cff2e20382
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2017-12-18 13:40:33 +0000

    first commit

---
+++ flappy-game/index.js
@@ -0,0 +1,13 @@
+//Get the page to respond to jQuery
+
+//Make the character move up and down when selected 
+
+// Get the background to move when bird moves 
+
+//Increase the speed as the game progresses 
+
+// Record the scores when character moves in between the bars 
+
+// Resets the game when character hits the bars and alert losing message
+
+// Give a winning score
